cce5e0ac2b090b5ff3058d4d44791ac703303b95
juan-augusto <juanaugusto1@live.com>
2022-09-26 11:47:20 -0300

    Teste code commit

--- CategorizeNewMember/CategorizeNewMember.js
+++ CategorizeNewMember/CategorizeNewMember.js
@@ -11,6 +11,7 @@
 // Example
 // input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+// FUNDAMENTALSARRAYS
 
 function openOrSenior(data){
     const arr = []
